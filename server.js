221cf3a44644ca7629cc22be97677e2d6d90fc83
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2023-07-19 20:22:37 -0500

    Added switch case statemnt for different cases

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const db = mysql.createConnection(
       database: 'Employee_MngDB'
     },
     console.log(`Connected to the Employee_MngDB database.`),
-    
+
     // Call first prompt
     initialPrompt()
 );
@@ -22,15 +22,77 @@ function initialPrompt() {
     .prompt({
         type: "list",
         choices: [
-            "Add department",
-            "Add role",
-            "Add employee",
             "View departments",
             "View roles",
             "View employees",
+            "Add departments",
+            "Add roles",
+            "Add employees",
             "Quit"
         ],
         message: "What would you like to do?",
         name: "option"
     })
+    .then(function ({ option }) {
+        switch (option) {
+            case "View departments":
+            viewDepartments();
+            break;
+  
+            case "View roles":
+            viewRoles();
+            break;
+        
+            case "View employees":
+            viewEmployees();
+            break;
+  
+            case "Add departments":
+            addDepartments();
+            break;
+  
+            case "Add roles":
+            addRoles();
+            break;
+  
+            case "Add employees":
+            addEmployees();
+            break;
+  
+            case "Quit":
+            db.end((err) => {
+                if (err) {
+                    console.error('Error closing the database connection:', err);
+                } else {
+                    console.log('Connection to the database has been closed.');
+                }
+                });
+            break;
+        }
+    });
+};
+
+//Function to view departments
+function viewDepartments() {
+    console.log("v dep");
+};
+// Function to view roles
+function viewRoles() {
+    console.log("v role");
+};
+// Function to view employees
+function viewEmployees() {
+    console.log("v empl");
+};
+// Function to add departments
+function addDepartments() {
+    console.log("add dep");
+};
+// Function to add roles
+function addRoles() {
+    console.log("add role");
+};
+// Function to add employees
+function addEmployees() {
+    console.log("add empl");
 };
